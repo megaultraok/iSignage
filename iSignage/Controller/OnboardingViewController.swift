e1414b27e882728a97f869f352c2c25477fbfdd3
Jada White <jadaw.2021@gmail.com>
2021-08-05 15:52:14 -0400

    Added starting main storyboard elements and connected status buttons to view controller code.

---
+++ iSignage/Controller/OnboardingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingViewController.swift
+//  iSignage
+//
+//  Created by Jada White on 8/5/21.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // When the status button is pressed, segue to the next view with text and color
+    @IBAction func statusButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
+
