0f90f3519b89edf719eed2a74b2e62f5bff8ac8a
Jada White <jadaw.2021@gmail.com>
2021-08-05 20:59:43 -0400

    Segue from Onboarding View Controller to Sign View Controller.

--- iSignage/Controller/OnboardingViewController.swift
+++ iSignage/Controller/OnboardingViewController.swift
@@ -8,16 +8,21 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
-
+    @IBOutlet weak var chillinButton: UIButton!
+    @IBOutlet weak var sleepingButton: UIButton!
+    @IBOutlet weak var workingButton: UIButton!
+    @IBOutlet weak var streamingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     // When the status button is pressed, segue to the next view with text and color
-    @IBAction func statusButtonPressed(_ sender: Any) {
-        
+    @IBAction func statusButtonPressed(_ sender: UIButton) {
+        let signvc = SignViewController()
+        signvc.view.backgroundColor = sender.currentTitleColor
+        performSegue(withIdentifier: "OnboardingToSign", sender: sender)
     }
-    
 }
 
---
+++ iSignage/Controller/SignViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignViewController.swift
+//  iSignage
+//
+//  Created by Jada White on 8/5/21.
+//
+
+import UIKit
+
+class SignViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
